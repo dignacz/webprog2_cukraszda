0119b77bfe17bbf0f24b05a70c1c75887a8aa965
skafula <79100340+skafula@users.noreply.github.com>
2022-11-28 19:19:23 +0100

    főoldal

--- home.php
+++ home.php
@@ -1,17 +1,17 @@
 <div class="container">
   <div class="row">
-<p><strong>Cukrászdánk</strong></p>
-<p>A Fiktív Cukrászda egy kitalált cukrászda, ahol nem létező süteményeket árulunk. Korlátlanul lehet itt a süteményt fogyasztani, hiszen nem fog meglátszódni a kilónkon, és a nulla cukortartalom pedig hozzájárul egészségünkhöz.</p>
-<p>Cukrászdánkban garantáltan megtalálja kedvencét, finomabbnál finomabb nem létező kínálatunk közül! </p>
-<div class="alert alert-info align=center;">Kínálatunk</div>
-			<div style="border:1px solid #000; padding:4px; margin:10px;">
-				<a href="images/torta1.png"><img src="images/torta1.png" style="max-width: 300px"/></a>
-                        <a href="images/torta2.jpg"><img src="images/torta2.jpg" style="max-width: 300px"/></a>
-                        <a href="images/torta3.jpg"><img src="images/torta3.jpg" style="max-width: 300px"/></a>
-                        <a href="images/torta4.jpg"><img src="images/torta4.jpg" style="max-width: 300px"/></a>
-                        <a href="images/torta5.jpg"><img src="images/torta5.jpg" style="max-width: 300px"/></a>
-                       
-			</div>
-	</div>
+    <p><strong>Cukrászdánk</strong></p>
+    <p>A Fiktív Cukrászda egy kitalált cukrászda, ahol nem létező süteményeket árulunk. Korlátlanul lehet itt a
+      süteményt fogyasztani, hiszen nem fog meglátszódni a kilónkon, és a nulla cukortartalom pedig hozzájárul
+      egészségünkhöz.</p>
+    <p>Cukrászdánkban garantáltan megtalálja kedvencét, finomabbnál finomabb nem létező kínálatunk közül! </p>
+    <div class="alert alert-info">Kínálatunk</div>
+    <div style="border:1px solid #000; padding:4px; margin:10px;">
+      <a href="images/torta1.png"><img src="images/torta1.png" style="max-width: 300px" /></a>
+      <a href="images/torta2.jpg"><img src="images/torta2.jpg" style="max-width: 300px" /></a>
+      <a href="images/torta3.jpg"><img src="images/torta3.jpg" style="max-width: 300px" /></a>
+      <a href="images/torta4.jpg"><img src="images/torta4.jpg" style="max-width: 300px" /></a>
+      <a href="images/torta5.jpg"><img src="images/torta5.jpg" style="max-width: 300px" /></a>
     </div>
-    </div>
+  </div>
+</div>
